ddad9ad7b1b43439c1c8eae775a99795de744340
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-02-09 10:10:04 -0500

    added create, delete user

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,7 +1,27 @@
 function UserServiceClient() {
-  this.fetchCourses = fetchUsers;
-  var self = this;
-  this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+  this.fetchUsers = fetchUsers;
+  this.createUser = createUser;
+  this.deleteUser = deleteUser;
+  const self = this;
+  this.url = 'https://wbdv-generic-server.herokuapp.com/api/001327792/users';
+
+  function createUser(user) {
+    return fetch(self.url, {
+      method: 'POST',
+      headers:{
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(function (response) {
+      return response.json()
+    })
+  }
+
+  function deleteUser(id) {
+    return fetch(`${self.url}/${id}`, {
+      method: 'DELETE'
+    })
+  }
 
   function fetchUsers() {
     return fetch(self.url).then(function (response) {
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,5 +1,43 @@
-var $theTableBody;
-var userService = new UserServiceClient();
+let $theTableBody;
+let $usernameFld;
+let $pwdFld;
+let $firstNameFld;
+let $lastNameFld;
+let $roleSelect;
+let $createBtn;
+let $deleteBtn;
+let users = [];
+// const self = this;
+const userService = new UserServiceClient();
+
+function addUser() {
+  createUser({
+    username: 'newuser',
+    password: 'newpwd',
+    firstname: 'new',
+    lastname: 'user',
+    role: 'fac'
+  })
+}
+
+function createUser(user) {
+  return userService.createUser(user).then(function (createdUser) {
+    users.push(createdUser);
+    renderUsers(users);
+  })
+}
+
+function deleteUser(event) {
+  let deleteBtn = $(event.target);
+  let theClass = deleteBtn.attr("class");
+  let theIndex = deleteBtn.attr("id");
+  let theId = users[theIndex]._id;
+
+  userService.deleteUser(theId).then(function (status) {
+    users.splice(theIndex, 1);
+    renderUsers(users)
+  })
+}
 
 function renderUsers(users) {
   $theTableBody.empty();
@@ -8,33 +46,50 @@ function renderUsers(users) {
     $theTableBody.prepend(`
       <tr>
         <td>${user.username}</td>
-        <td>${user.username}</td>
-        <td>${user.username}</td>
-        <td>${user.username}</td>
-        <td>${user.username}</td>
+        <td>${user.password}</td>
+        <td>${user.firstname}</td>
+        <td>${user.lastname}</td>
+        <td>${user.role}</td>
         <td>
           <div class="row">
             <div class="col-6">
-              <i class="fas fa-times"></i>
+              <button class="fas fa-times wbdv-useradmin-delete-user" id="${i}"></button>
             </div>
             <div class="col">
-              <i class="fas fa-pen"></i>
+              <button class="fas fa-pen" id="${user._id}"></button>
             </div>
           </div>
         </td>
     </tr>
     `)
   }
-  userService.fetchCourses().then(function (value) {
-    console.log(value)
-  })
+  $(".wbdv-useradmin-delete-user").click(deleteUser);
 }
 
 function init() {
+  $usernameFld = $(".wbdv-useradmin-input-username");
+  $pwdFld = $(".wbdv-useradmin-input-password");
+  $firstNameFld = $(".wbdv-useradmin-input-firstname");
+  $lastNameFld = $(".wbdv-useradmin-input-lastname");
+  $roleSelect = $(".wbdv-useradmin-input-role");
   $theTableBody = jQuery("tbody");
-  userService.fetchCourses().then(function (users) {
+  $createBtn = $(".wbdv-useradmin-create-btn");
+  $deleteBtn = $(".wbdv-useradmin-delete-user");
+
+  userService.fetchUsers().then(function (fetchedUsers) {
+    users = fetchedUsers;
     renderUsers(users)
-  })
+  });
+
+  $createBtn.click(() => createUser({
+    username: $usernameFld.val(),
+    password: $pwdFld.val(),
+    firstname: $firstNameFld.val(),
+    lastname: $lastNameFld.val(),
+    role: $roleSelect.val()
+  }))
+
+
 }
 
 jQuery(init);
